b0bfdc7b9cfd58985110d94409bcc9bee7d81461
Elhay Efrat <elhayef@monday.com>
2024-01-07 13:10:45 +0200

    sdf

--- genaiRouter.py
+++ genaiRouter.py
@@ -17,12 +17,14 @@ def generic_azure_openai_client(openai_api_key,open_ai_model,messages,openai_api
                 azure_endpoint=openai_api_base,
                 api_key=openai_api_key
         )
-        return = client.chat.completions.create(
+        res = client.chat.completions.create(
             model=open_ai_model,
             messages=messages,
             temperature=model_temperature,
             max_tokens=max_prompt_tokens
         )
+        print(res)
+        return res
     except Exception as e:
         print(f'error :{e}')
         return 'Failed'
